8e561014e59dc43162c2fb0dfc4ad7f240d90e80
Nicoli <103062064+ItsNiSantana@users.noreply.github.com>
2024-03-01 16:11:07 -0300

    Update exe3.php

--- entrada/exe3.php
+++ entrada/exe3.php
@@ -7,38 +7,41 @@ echo $idade . "\n";
 echo $salario . "\n";
 
 /*INSS*/
-if ($salario>=7786.02):
-    echo "INSS isento";
-    $salariosmodificado = $salario; 
+if ($salario>=7786.03):
+    echo "INSS isento"; 
 elseif($salario>=4000.04 && $salario<=7786.02):
-    $salariomodificado = ($salario * 0.14)-181.18;
-    echo "INSS descontado";
+    $inss = ($salario * 0.14) - 181.18;
+    echo "INSS = ", $inss;
 elseif($salario>=2666.69 && $salario<=4000.03):
-    $salariomodificado = ($salario * 0.12)-101.18;
-    echo "INSS descontado";
+    $inss = ($salario * 0.12) - 101.18;
+    echo "INSS = ", $inss;
 elseif($salario>=1412.01 && $salario<=2666.68):
-    $salariomodificado = ($salario * 0.12)-101.18;
-    echo "INSS descontado";
-elseif($salario<=2259.20):
-    $salariomodificado = $salario * 0.075;
-    echo "INSS descontado";
+    $inss = ($salario * 0.09) - 21.18;
+    echo "INSS = ", $inss;
+elseif($salario<=1412.00):
+    $inss = $salario * 0.075;
+    echo "INSS = ", $inss;
 endif;
 
+$salariomodificado = $salario - $inss;
+
 if ($salario>=4664.68):
-    echo "INSS isento";
-    $salariosmodificado = $salario; 
-elseif($salario>=4000.04 && $salario<=7786.02):
-    $salariomodificado = ($salario * 0.14)-181.18;
-    echo "INSS descontado";
-elseif($salario>=2666.69 && $salario<=4000.03):
-    $salariomodificado = ($salario * 0.12)-101.18;
-    echo "INSS descontado";
-elseif($salario>=1412.01 && $salario<=2666.68):
-    $salariomodificado = ($salario * 0.12)-101.18;
-    echo "INSS descontado";
+    $ir = $salariomodificado * 0.275;
+    echo "IRPF = ", $ir;
+elseif($salario>=3751.06 && $salario<=4664.68):
+    $ir = $salariomodificado * 0.225;
+    echo "IRPF = ", $ir;
+elseif($salario>=2826.66 && $salario<=3751.05):
+    $ir = $salariomodificado * 0.15;
+    echo "IRPF = ", $ir;
+elseif($salario>=2259.21 && $salario<=2826.65):
+    $ir = $salariomodificado * 0.075;
+    echo "IRPF = ", $ir;
 elseif($salario<=2259.20):
-    $salariomodificado = $salario * 0.075;
-    echo "INSS descontado";
+    echo "IRPF Isento";
 endif;
+
+$salariofinal = $salariomodificado - ir;
+echo "Seu salario final = ", $salariofinal;
 
 ?>
