23623acd4b691a9f0dd141682cd0095844730de8
Nicoli <103062064+ItsNiSantana@users.noreply.github.com>
2024-03-26 11:56:17 -0300

    Update 1094.php

--- lista 3/1094.php
+++ lista 3/1094.php
@@ -1,23 +1,29 @@
 <?php
-$numero_insercoes = intval(readline());
-$qtd_ratos = 0;
-$qtd_sapos = 0;
-$qtd_coelhos = 0;
-$qtd_cobaias = 0;
-for ($i = 1; $i <= $numero_insercoes; $i++) {
+$n = intval(readline());
+$ratos = 0;
+$sapos = 0;
+$coelhos = 0;
+$cobaias = 0;
+for ($i = 1; $i <= $n; $i++) {
     list($qtd, $animal) = explode(" ", readline());
-    $qtd_cobaias += $qtd;
+    $cobaias += $qtd;
     if ($animal == 'C')
-        $qtd_coelhos += $qtd;
+        $coelhos += $qtd;
     elseif ($animal == 'S')
-        $qtd_sapos += $qtd;
+        $sapos += $qtd;
     elseif ($animal == 'R')
-        $qtd_ratos += $qtd;
+        $ratos += $qtd;
 }
-echo "Total: $qtd_cobaias cobaias\n";
-echo "Total de coelhos: $qtd_coelhos\n";
-echo "Total de ratos: $qtd_ratos\n";
-echo "Total de sapos: $qtd_sapos\n";
-echo "Percentual de coelhos: " . number_format((($qtd_coelhos / $qtd_cobaias) * 100), 2, '.', '') . " %\n";
-echo "Percentual de ratos: " . number_format((($qtd_ratos / $qtd_cobaias) * 100), 2, '.', '') . " %\n";
-echo "Percentual de sapos: " . number_format((($qtd_sapos / $qtd_cobaias) * 100), 2, '.', '') . " %\n";
+
+$pcoelhos = ($coelhos / $cobaias) * 100;
+$pratos = ($ratos / $cobaias) * 100;
+$psapos = ($sapos / $cobaias) * 100;
+
+echo "Total: ", $cobaias, " cobaias\n";
+echo "Total de coelhos: ", $coelhos, "\n";
+echo "Total de ratos: ", $ratos, "\n";
+echo "Total de sapos: ", $sapos, "\n";
+echo "Percentual de coelhos: ", number_format(($pcoelhos), 2, '.', '') . " %\n";
+echo "Percentual de ratos: ", number_format(($pratos), 2, '.', '') . " %\n";
+echo "Percentual de sapos: ", number_format(($psapos), 2, '.', '') . " %\n";
+?>
