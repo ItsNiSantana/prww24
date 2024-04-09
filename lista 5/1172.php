7fa5752498179cb66ec02d9e4e881ab1563d90a9
Nicoli <103062064+ItsNiSantana@users.noreply.github.com>
2024-04-09 09:40:03 -0300

    Update 1172.php

--- lista 5/1172.php
+++ lista 5/1172.php
@@ -1,14 +1,12 @@
 <?php
+$x = [];
 
-    $x = [];
+for ($i = 0; $i < 10; $i++) {
+    $valor = intval(fgets(STDIN));
+    $x[$i] = $valor <= 0 ? 1 : $valor;
+}
 
-    for($i = 0; $i < 10; $i++) {
-        $valor = intval(trim(fgets(STDIN)));
-        $x[] = max(1, $valor);
-    }
-    
-    for($i = 0; $i < 10; $i++) {
-        echo "X[$i] = {$x[$i]}\n";
-    }
-
+foreach ($x as $n => $x) {
+    echo "X[$n] = $x\n";
+}
 ?>
